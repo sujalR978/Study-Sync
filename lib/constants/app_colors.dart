c84352d7db6fadeec8a4a746322b3cfe7a3ca4fe
sujalR978 <srashiya955@rku.ac.in>
2026-05-27 10:38:53 +0530

    #

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFF0052FF);
+  static const Color secondary = Color(0xFF00D1FF);
+  static const Color tertiary = Color(0xFF7000FF);
+
+  static const Color background = Color(0xFF0F172A);
+  static const Color cardColor = Color(0xFF1E293B);
+
+  static const Color white = Colors.white;
+  static const Color greyText = Color(0xFF94A3B8);
+}
