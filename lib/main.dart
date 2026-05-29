c6e5c72ff7b3876629c4771ff2ea06cf0c937a94
sujalR978 <srashiya955@rku.ac.in>
2026-05-29 11:01:45 +0530

    #

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:study_sync/firebase_options.dart';
 import 'package:study_sync/screens/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
