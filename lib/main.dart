54962be9258a8f7ca52b7096c35ca1538db80888
sujalR978 <srashiya955@rku.ac.in>
2026-05-30 17:26:14 +0530

    #

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
+      debugShowCheckedModeBanner: false,
       home: const SplashScreen(),
     );
   }
