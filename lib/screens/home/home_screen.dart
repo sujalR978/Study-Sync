6c562458df95e036151d8dc265ca56f8a10ac7c9
sujalR978 <srashiya955@rku.ac.in>
2026-05-30 12:46:32 +0530

    #

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:study_sync/screens/auth/logout_screen.dart';
+import 'package:study_sync/screens/profile/profileScreen.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,6 +12,29 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(
+                  context,
+                ).push(MaterialPageRoute(builder: (context) => LogoutScreen()));
+              },
+              child: Text('logout'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).push(
+                  MaterialPageRoute(builder: (context) => Profilescreen()),
+                );
+              },
+              child: Text('profile'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
