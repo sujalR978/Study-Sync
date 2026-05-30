daba02ab6abe054f808070a6c0a6567811a16844
sujalR978 <srashiya955@rku.ac.in>
2026-05-30 16:42:02 +0530

    #

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -432,7 +432,7 @@ class _LoginScreenState extends State<LoginScreen> {
                                   Navigator.pushReplacement(
                                     context,
                                     MaterialPageRoute(
-                                      builder: (_) => googleInfoscreen(),
+                                      builder: (_) => GoogleInfoScreen(),
                                     ),
                                   );
                                 } else {
