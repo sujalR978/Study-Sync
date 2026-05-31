bccd75b77180da5e56ebe3e14ab69312f1b8f1ad
sujalR978 <srashiya955@rku.ac.in>
2026-05-31 15:41:11 +0530

    #

--- lib/screens/profile/profileScreen.dart
+++ lib/screens/profile/profileScreen.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:study_sync/models/user_model.dart';
+import 'package:study_sync/providers/auth_provider.dart';
 import 'package:study_sync/screens/profile/editProfileScreen.dart';
 import 'package:study_sync/services/auth_service.dart';
 
@@ -28,6 +31,10 @@ class _ProfilescreenState extends State<Profilescreen> {
               return const Text('No data found');
             }
             UserModel? user = snapshot.data!;
+
+            if (user != null) {
+              context.read<Authprovider>().setUser(user);
+            }
 
             return Center(
               child: Column(
