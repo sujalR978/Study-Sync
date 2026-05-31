b014bffc6f7050807cbffd92973db7d8086be437
sujalR978 <srashiya955@rku.ac.in>
2026-05-31 16:44:36 +0530

    #

---
+++ lib/services/update_auth_data.dart
@@ -0,0 +1,31 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:study_sync/models/update_model.dart';
+
+class UpdateAuthData {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<void> updateAuthData ( {
+    required fullname,
+    required username,
+    required phone,
+    required photoUrl,
+    
+  }) async{
+    String uid = _auth.currentUser!.uid;
+
+    UpdateProfileData updateUser = UpdateProfileData(
+      fullname: fullname,
+      username: username,
+      phone: phone,
+      photoUrl: photoUrl,
+      updatedAt: DateTime.now(),
+    );
+
+    await _firestore
+        .collection('users')
+        .doc(uid)
+        .update(updateUser.updateMap());
+  }
+}
