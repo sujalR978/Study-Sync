096de6bf0aa820108dfa5cca4b375b7a2bae66d2
sujalR978 <srashiya955@rku.ac.in>
2026-05-31 15:41:04 +0530

    #

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,23 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:study_sync/firebase_options.dart';
+import 'package:provider/provider.dart';
+import 'package:study_sync/providers/auth_provider.dart';
 import 'package:study_sync/screens/splash/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+ 
+ MultiProvider(
+  providers: [
+    ChangeNotifierProvider(
+      create: (_) => Authprovider(),
+    ),
+  ],
+ 
+child:  const MyApp(),
+ );
 }
 
 class MyApp extends StatelessWidget {
