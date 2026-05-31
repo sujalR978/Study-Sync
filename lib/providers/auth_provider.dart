5d02c263eea2f446334f9232cb6cd7a4172a9097
sujalR978 <srashiya955@rku.ac.in>
2026-05-31 15:41:06 +0530

    #

---
+++ lib/providers/auth_provider.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:study_sync/models/user_model.dart';
+
+class Authprovider extends ChangeNotifier {
+  UserModel? _user;
+
+  UserModel? get user => _user;
+
+  void setUser(UserModel userData) {
+    _user = userData;
+    notifyListeners();
+  }
+
+  void clearUser() {
+    _user = null;
+    notifyListeners();
+  }
+}
