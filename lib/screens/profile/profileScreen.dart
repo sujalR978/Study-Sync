eaf4c7add6dd8695e5108cb48b545da0cce73c98
sujalR978 <srashiya955@rku.ac.in>
2026-05-31 11:03:22 +0530

    #

--- lib/screens/profile/profileScreen.dart
+++ lib/screens/profile/profileScreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:study_sync/models/user_model.dart';
+import 'package:study_sync/services/auth_service.dart';
 
 class Profilescreen extends StatefulWidget {
   const Profilescreen({super.key});
@@ -8,24 +10,36 @@ class Profilescreen extends StatefulWidget {
 }
 
 class _ProfilescreenState extends State<Profilescreen> {
-  String fullname = 'fgnfd';
-  String username = ' fd';
-  String email = 'dfsd@gamil.com';
-  String phone = '1234567890';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(fullname),
-              Text(username),
-              Text(email),
-              Text(phone),
-            ],
-          ),
+        child: FutureBuilder(
+          future: AuthService().getCurrentUserData(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const CircularProgressIndicator();
+            }
+            if (snapshot.hasError) {
+              return Text(snapshot.error.toString());
+            }
+            if (!snapshot.hasData) {
+              return const Text('No data found');
+            }
+            UserModel? user = snapshot.data!;
+
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(user.fullname),
+                  Text(user.username),
+                  Text(user.email),
+                  Text(user.phone),
+                ],
+              ),
+            );
+          },
         ),
       ),
     );
