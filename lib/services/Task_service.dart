57d8124fb3040624113800720717203896a0b015
sujalR978 <srashiya955@rku.ac.in>
2026-06-01 17:10:27 +0530

    #

---
+++ lib/services/Task_service.dart
@@ -0,0 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class TaskService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<void> addTask()async {
+
+  }
+}
