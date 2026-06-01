d4bc2cf99769d17931e4a7ab511f2ec27c0c7dd3
sujalR978 <srashiya955@rku.ac.in>
2026-06-01 14:17:42 +0530

    #

---
+++ lib/models/Task_model.dart
@@ -0,0 +1,45 @@
+class TaskModel {
+  String id;
+  String title;
+  String description;
+  String category;
+  String priority;
+  DateTime dueDate;
+  String dueTime;
+
+  bool isCompleted;
+  int order;
+
+  DateTime createdAt;
+  DateTime updatedAt;
+  TaskModel({
+    required this.id,
+
+    required this.title,
+    required this.description,
+    required this.category,
+    required this.priority,
+    required this.dueDate,
+    required this.dueTime,
+    required this.isCompleted,
+    required this.order,
+    required this.createdAt,
+    required this.updatedAt,
+  });
+
+  Map<dynamic, dynamic> TaskMap() {
+    return {
+      'id' : id,
+      'title' : title,
+      'description' : description,
+      'category' : category,
+      'priority' : priority,
+      'dueDate' : dueDate,
+      'dueTime' : dueTime,
+      'isCompleted' : isCompleted,
+      'order' : order,
+      'createdAt' : createdAt,
+      'updatedAt':updatedAt,
+    };
+  }
+}
