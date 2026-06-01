793f0479ac866e8260b2f9fd07aa2841cdac4670
sujalR978 <srashiya955@rku.ac.in>
2026-06-01 16:19:27 +0530

    #

--- lib/widgets/category_card.dart
+++ lib/widgets/category_card.dart
@@ -1,15 +1,77 @@
 import 'package:flutter/material.dart';
 
-class category_card extends StatefulWidget {
-  const category_card({super.key});
+class CategoryCard extends StatelessWidget {
+  final String category;
+  final bool isSelected;
+  final VoidCallback onTap;
+  final VoidCallback onLongPress;
 
-  @override
-  State<category_card> createState() => _category_cardState();
-}
+  const CategoryCard({
+    super.key,
+    required this.category,
+    required this.isSelected,
+    required this.onTap,
+    required this.onLongPress,
+  });
 
-class _category_cardState extends State<category_card> {
+  IconData getIcon() {
+    switch (category) {
+      case "Study":
+        return Icons.school_outlined;
+      case "Work":
+        return Icons.work_outline_rounded;
+      case "Personal":
+        return Icons.person_outline_rounded;
+      case "Health":
+        return Icons.favorite_border_rounded;
+      case "Shopping":
+        return Icons.shopping_cart_outlined;
+      default:
+        return Icons.category_outlined;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(height: 30, width: 60, color: Colors.amber);
+    return GestureDetector(
+      onTap: onTap,
+      onLongPress: onLongPress,
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 250),
+        height: 60,
+        decoration: BoxDecoration(
+          color: isSelected ? const Color(0xFF4F46E5) : const Color(0xFFE9EDF7),
+          borderRadius: BorderRadius.circular(30),
+          boxShadow: isSelected
+              ? [
+                  BoxShadow(
+                    color: const Color(0xFF4F46E5).withOpacity(0.25),
+                    blurRadius: 10,
+                    offset: const Offset(0, 4),
+                  ),
+                ]
+              : [],
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              getIcon(),
+              color: isSelected ? Colors.white : const Color(0xFF4B5563),
+              size: 18,
+            ),
+            const SizedBox(width: 10),
+            Text(
+              category,
+              style: TextStyle(
+                color: isSelected ? Colors.white : const Color(0xFF4B5563),
+                fontWeight: FontWeight.w600,
+                fontSize: 14,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
