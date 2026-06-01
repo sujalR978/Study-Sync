4a03fb44ae5176378275fb6ed0bd71bfe5bacefd
sujalR978 <srashiya955@rku.ac.in>
2026-06-01 16:19:24 +0530

    #

--- lib/screens/Task Screen/add_Task_Screen.dart
+++ lib/screens/Task Screen/add_Task_Screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:study_sync/widgets/category_card.dart';
 import 'package:study_sync/widgets/custom_textfield.dart';
+import 'package:custom_popup_dialog/custom_popup_dialog.dart';
 
 class AddTaskScreen extends StatefulWidget {
   const AddTaskScreen({super.key});
@@ -12,7 +13,18 @@ class AddTaskScreen extends StatefulWidget {
 }
 
 class _AddTaskScreenState extends State<AddTaskScreen> {
+  final TextEditingController newcategory = TextEditingController();
+  final GlobalKey<FormState> keyForm = GlobalKey<FormState>();
+  List<String> category = ["Study", "Work", "Personal", "Health", "Shopping"];
   String _selectdvalue = '';
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    newcategory.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,32 +60,89 @@ class _AddTaskScreenState extends State<AddTaskScreen> {
                 },
               ),
               Text('Category'),
-              RadioGroup(
-                groupValue: _selectdvalue,
-                onChanged: (String? value) {
-                  setState(() {
-                    _selectdvalue = value!;
-                  });
-                },
-                child: Container(
-                  height: 500,
-                  child: Column(
-                    children: [
-                      ListView.builder(
-                        itemCount: 9,
-                        itemBuilder: (context, index) {
-                          return category_card();
-                        },
-                      ),
-                      Radio(value: 'value'),
-                      Text('value'),
-                      Radio(value: 'hi'),
-                      Text('hi'),
+              TextButton(
+                onPressed: () {
+                  showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: const Text('Add Category'),
+                        content: Form(
+                          key: keyForm,
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              CustomTextfield.customTextField(
+                                hintText: 'Enter category',
+                                icon: Icons.category,
+                                controller: newcategory,
+                                valideter: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Enter Category';
+                                  }
+                                  return null;
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                        actions: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.pop(context);
+                            },
+                            child: const Text('Cancel'),
+                          ),
 
-                      Radio(value: 'hello'),
-                      Text('hello'),
-                    ],
+                          ElevatedButton(
+                            onPressed: () {
+                              if (keyForm.currentState!.validate()) {
+                                setState(() {
+                                  category.add(newcategory.text);
+                                });
+
+                                Navigator.pop(context);
+                                newcategory.clear();
+                              }
+                            },
+                            child: const Text('Save'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
+                },
+                child: const Text('Add Category'),
+              ),
+
+              SizedBox(
+                width: 350,
+                child: GridView.builder(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  itemCount: category.length,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                    mainAxisSpacing: 12,
+                    crossAxisSpacing: 6,
+                    childAspectRatio: 2.7,
                   ),
+                  itemBuilder: (context, index) {
+                    return CategoryCard(
+                      category: category[index],
+                      isSelected: _selectdvalue == category[index],
+                      onTap: () {
+                        setState(() {
+                          _selectdvalue = category[index];
+                        });
+                      },
+                      onLongPress: () {
+                        setState(() {
+                          category.removeAt(index);
+                        });
+                      },
+                    );
+                  },
                 ),
               ),
             ],
