2fb7ce89387aedf1625b02cb814e3824f6f874a3
sujalR978 <srashiya955@rku.ac.in>
2026-06-02 17:20:28 +0530

    #

--- lib/models/Task_model.dart
+++ lib/models/Task_model.dart
@@ -1,3 +1,5 @@
+import 'package:flutter/material.dart';
+
 class TaskModel {
   String id;
   String title;
@@ -29,17 +31,50 @@ class TaskModel {
 
   Map<String, dynamic> TaskMap() {
     return {
-      'id' : id,
-      'title' : title,
-      'description' : description,
-      'category' : category,
-      'priority' : priority,
-      'dueDate' : dueDate,
-      'dueTime' : dueTime,
-      'isCompleted' : isCompleted,
-      'order' : order,
-      'createdAt' : createdAt,
-      'updatedAt':updatedAt,
+      'id': id,
+      'title': title,
+      'description': description,
+      'category': category,
+      'priority': priority,
+      'dueDate': dueDate,
+      'dueTime': dueTime,
+      'isCompleted': isCompleted,
+      'order': order,
+      'createdAt': createdAt,
+      'updatedAt': updatedAt,
+    };
+  }
+}
+
+class updateTaskModel {
+  String title;
+  String description;
+  String category;
+  String priority;
+  DateTime dueDate;
+  String dueTime;
+
+  DateTime updatedAt;
+
+  updateTaskModel({
+    required this.title,
+    required this.description,
+    required this.category,
+    required this.priority,
+    required this.dueDate,
+    required this.dueTime,
+    required this.updatedAt,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'description': description,
+      'category': category,
+      'priority': priority,
+      'dueDate': dueDate,
+      'dueTime': dueTime,
+      'updatedAt': updatedAt,
     };
   }
 }
