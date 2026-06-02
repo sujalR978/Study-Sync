76201765371be8f3856755acbffeed78935774fa
sujalR978 <srashiya955@rku.ac.in>
2026-06-02 10:52:28 +0530

    #

---
+++ lib/screens/BottomNavigation.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:study_sync/screens/Task%20Screen/add_Task_Screen.dart';
+import 'package:study_sync/screens/home/home_screen.dart';
+import 'package:study_sync/screens/profile/profileScreen.dart';
+
+class Bottomnavigation extends StatefulWidget {
+  const Bottomnavigation({super.key});
+
+  @override
+  State<Bottomnavigation> createState() => _MainState();
+}
+
+class _MainState extends State<Bottomnavigation> {
+  int currentIndex = 0;
+
+  final List<Widget> page = [
+    const HomeScreen(),
+    const AddTaskScreen(),
+    const Profilescreen(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: page[currentIndex],
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (value) {
+          setState(() {
+            currentIndex = value;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_task),
+            label: "Add Task",
+          ),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+        ],
+      ),
+    );
+  }
+}
