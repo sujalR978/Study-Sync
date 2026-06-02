3dd84a4aaaf2e5aed89a4c62ebd7d2c5461a0970
sujalR978 <srashiya955@rku.ac.in>
2026-06-02 14:40:44 +0530

    #

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:study_sync/models/Task_model.dart';
 import 'package:study_sync/screens/Task%20Screen/add_Task_Screen.dart';
 import 'package:study_sync/screens/auth/googleInfo_screen.dart';
 import 'package:study_sync/screens/auth/logout_screen.dart';
 import 'package:study_sync/screens/profile/profileScreen.dart';
+import 'package:study_sync/services/Task_service.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -33,6 +35,31 @@ class _HomeScreenState extends State<HomeScreen> {
                 );
               },
               child: Text('Task add'),
+            ),
+
+            StreamBuilder(
+              stream: TaskService().getTask(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                if (snapshot.hasError) {
+                  return Center(child: Text(snapshot.error.toString()));
+                }
+                if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+                  return const Center(child: Text('No Tasks Found'));
+                }
+
+                final tasks = snapshot.data!.docs;
+
+                return ReorderableListView(
+                  children: [
+                    for (final task in tasks)
+                      ListTile(key: ValueKey(task), title: Text(task['title'])),
+                  ],
+                  onReorder: (oldindex, newindex) => () {},
+                );
+              },
             ),
           ],
         ),
