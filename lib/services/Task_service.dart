e9f950f148d18a9533b023e9517f6717daef180b
sujalR978 <srashiya955@rku.ac.in>
2026-06-02 10:04:25 +0530

    #

--- lib/services/Task_service.dart
+++ lib/services/Task_service.dart
@@ -1,11 +1,56 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:study_sync/models/Task_model.dart';
 
 class TaskService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
-  Future<void> addTask()async {
+  Future<void> addTask({
+    required String title,
+    required String description,
+    required String category,
+    required String priority,
+    required DateTime dueDate,
+    required String dueTime,
+  }) async {
+    String uid = _auth.currentUser!.uid;
 
+    // get Task id
+    CollectionReference Task = _firestore
+        .collection('users')
+        .doc(uid)
+        .collection('tasks');
+    DocumentReference docRef = Task.doc();
+    String id = docRef.id;
+
+    // get order for reorderabel list
+    QuerySnapshot snapshot = await _firestore
+        .collection('users')
+        .doc(uid)
+        .collection('tasks')
+        .get();
+
+    int order = snapshot.docs.length;
+
+    TaskModel task = TaskModel(
+      id: id,
+      title: title,
+      description: description,
+      category: category,
+      priority: priority,
+      dueDate: dueDate,
+      dueTime: dueTime,
+      isCompleted: false,
+      order: order,
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+    );
+    await _firestore
+        .collection('users')
+        .doc(uid)
+        .collection('tasks')
+        .doc(id)
+        .set(task.TaskMap());
   }
 }
