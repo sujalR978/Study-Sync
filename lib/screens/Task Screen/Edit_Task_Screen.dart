6de46ec63818ef1cb06af5dfd6d8817f95bf0a52
sujalR978 <srashiya955@rku.ac.in>
2026-06-03 15:43:44 +0530

    #

--- lib/screens/Task Screen/Edit_Task_Screen.dart
+++ lib/screens/Task Screen/Edit_Task_Screen.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
+
 
 class EditTaskScreen extends StatefulWidget {
   final String taskid;
