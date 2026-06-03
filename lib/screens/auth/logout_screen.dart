cb4b36988944c63a13c72a86719287d7042d1ae2
sujalR978 <srashiya955@rku.ac.in>
2026-06-03 15:45:13 +0530

    #

--- lib/screens/auth/logout_screen.dart
+++ lib/screens/auth/logout_screen.dart
@@ -3,7 +3,7 @@ import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:study_sync/providers/auth_provider.dart';
 import 'package:study_sync/screens/auth/login_screen.dart';
-import 'package:study_sync/screens/home/home_screen.dart';
+
 import 'package:study_sync/screens/profile/profileScreen.dart';
 import 'package:study_sync/services/auth_service.dart';
 import 'package:study_sync/widgets/custom_button.dart';
