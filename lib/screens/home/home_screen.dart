484187e781add42f6bb58ff9526aa2b16daa1e5e
sujalR978 <srashiya955@rku.ac.in>
2026-06-03 15:42:29 +0530

    #

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -217,7 +217,7 @@ class _HomeScreenState extends State<HomeScreen> {
                           ),
                           child: ListTile(
                             onTap: () {
-                              Navigator.of(context).push(MaterialPageRoute(builder: (_)=>EditTaskScreen(taskid: task.id,)));
+                              Navigator.of(context).push(MaterialPageRoute(builder: (_)=>EditTaskScreen(taskid: task.i,)));
                             },
                             contentPadding: const EdgeInsets.symmetric(
                               horizontal: 16,
