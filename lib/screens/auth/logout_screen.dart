5a858f2afe2735debed80327a47204c5e52b7a9c
sujalR978 <srashiya955@rku.ac.in>
2026-06-03 15:45:23 +0530

    #

--- lib/screens/auth/logout_screen.dart
+++ lib/screens/auth/logout_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+
 import 'package:study_sync/providers/auth_provider.dart';
 import 'package:study_sync/screens/auth/login_screen.dart';
 
