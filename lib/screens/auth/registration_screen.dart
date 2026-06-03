e15953c792e09d251d14cb1f7bfd75cc29bd0cf2
sujalR978 <srashiya955@rku.ac.in>
2026-06-03 15:45:43 +0530

    #

--- lib/screens/auth/registration_screen.dart
+++ lib/screens/auth/registration_screen.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:study_sync/screens/auth/googleInfo_screen.dart';
 
 import 'package:study_sync/screens/auth/login_screen.dart';
 
