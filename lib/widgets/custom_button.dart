a1364c4dab035dfdf36bb7c0d11f8b1fafe5b004
sujalR978 <srashiya955@rku.ac.in>
2026-06-03 15:46:52 +0530

    #

--- lib/widgets/custom_button.dart
+++ lib/widgets/custom_button.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:study_sync/screens/home/home_screen.dart';
-import 'package:study_sync/services/auth_service.dart';
+
 
 class CustomButton {
   static gogalButton({required VoidCallback onPressed}) {
