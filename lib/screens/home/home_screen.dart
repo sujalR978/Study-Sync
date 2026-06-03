12ea60105a612b1e13288b931f11d99fa4755229
sujalR978 <srashiya955@rku.ac.in>
2026-06-03 15:43:02 +0530

    #

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -4,7 +4,6 @@ import 'package:study_sync/models/Task_model.dart';
 import 'package:study_sync/providers/auth_provider.dart';
 import 'package:study_sync/screens/Task%20Screen/Edit_Task_Screen.dart';
 import 'package:study_sync/screens/Task%20Screen/add_Task_Screen.dart';
-import 'package:study_sync/screens/auth/googleInfo_screen.dart';
 
 import 'package:study_sync/screens/profile/profileScreen.dart';
 import 'package:study_sync/services/Task_service.dart';
