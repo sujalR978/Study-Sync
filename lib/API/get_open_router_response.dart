bbc7a0a860e699edea81aa3f30099e017ad8d95c
sujalR978 <srashiya955@rku.ac.in>
2026-06-07 10:27:55 +0530

    #

--- lib/API/get_open_router_response.dart
+++ lib/API/get_open_router_response.dart
@@ -1,33 +1,35 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 import 'package:study_sync/API/api_key.dart';
 
 Future<String> getOpenRouterResponse(String userInput) async {
   const String endpoint = "https://openrouter.ai/api/v1/chat/completions";
 
-  final header = {
+  final headers = {
     'Authorization': 'Bearer $API',
     'Content-Type': 'application/json',
   };
 
   final body = jsonEncode({
-    'model': 'riverflow-v2.5-pro',
-    'prompt': userInput,
-    'max_tokens': 100,
-    'temperature': 0.7,
+    "model": "openai/gpt-3.5-turbo",
+    "messages": [
+      {"role": "user", "content": userInput},
+    ],
+    "temperature": 0.7,
+    "max_tokens": 200,
   });
 
-  final Response = await http.post(
+  final response = await http.post(
     Uri.parse(endpoint),
-    headers: header,
+    headers: headers,
     body: body,
   );
 
-  if (Response.statusCode == 200) {
-    final Map<String, dynamic> data = jsonDecode(Response.body);
+  if (response.statusCode == 200) {
+    final data = jsonDecode(response.body);
+
     return data['choices'][0]['message']['content'];
   } else {
-    throw Exception('Failed to get response ${Response.body}');
+    throw Exception('Failed: ${response.statusCode} - ${response.body}');
   }
 }
