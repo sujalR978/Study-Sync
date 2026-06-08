13e67e8049b244d4cc3d0293e8efb360a7f618e3
sujalR978 <srashiya955@rku.ac.in>
2026-06-08 16:33:44 +0530

    #

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:study_sync/constants/app_colors.dart';
 import 'package:study_sync/firebase_options.dart';
 import 'package:provider/provider.dart';
 import 'package:study_sync/providers/auth_provider.dart';
+import 'package:study_sync/providers/them_provider.dart';
 
 import 'package:study_sync/screens/splash/splash_screen.dart';
 import 'package:study_sync/services/notification_service.dart';
@@ -19,8 +21,13 @@ void main() async {
 
   runApp(
     MultiProvider(
-      providers: [ChangeNotifierProvider(create: (_) => Authprovider())],
-      child: const MyApp(),
+      providers: [
+        ChangeNotifierProvider(create: (_) => Authprovider()),
+        ChangeNotifierProvider(create: (_) => ThemeProvider()),
+      ],
+      builder: (context, child) {
+        return const MyApp();
+      },
     ),
   );
 }
@@ -30,13 +37,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final themProvider = context.watch<ThemeProvider>();
     return MaterialApp(
       title: 'Flutter Demo',
+
       theme: ThemeData(
-        // FIXED: Added 'ColorScheme' class before '.fromSeed'
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        brightness: Brightness.light,
+        scaffoldBackgroundColor: AppColors.background,
       ),
+
+      // --- CONFIGURING DARK MODE ---
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: AppColors.darkBackground,
+      ),
+
+      // ThemeMode.system adapts automatically if the phone toggles Dark Mode globally!
+      themeMode: themProvider.themeMode,
+
       debugShowCheckedModeBanner: false,
       home: const SplashScreen(),
     );
