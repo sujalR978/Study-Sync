dcd0c3a09758772c4b7c09734c7b3d3e31b65820
sujalR978 <srashiya955@rku.ac.in>
2026-06-09 16:01:26 +0530

    #

--- lib/screens/auth/logout_screen.dart
+++ lib/screens/auth/logout_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'package:study_sync/providers/auth_provider.dart';
+import 'package:study_sync/screens/BottomNavigation.dart';
 import 'package:study_sync/screens/auth/login_screen.dart';
 import 'package:study_sync/screens/profile/profileScreen.dart';
 import 'package:study_sync/services/auth_service.dart';
@@ -200,7 +201,7 @@ class _LogoutScreenState extends State<LogoutScreen> {
                                   Navigator.of(context).push(
                                     MaterialPageRoute(
                                       builder: (context) =>
-                                          const Profilescreen(),
+                                          const Bottomnavigation(),
                                     ),
                                   );
                                 },
