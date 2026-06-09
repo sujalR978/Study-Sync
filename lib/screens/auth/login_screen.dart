fb44449e22cdb88d685d545c3797c4dbfdaf0416
sujalR978 <srashiya955@rku.ac.in>
2026-06-09 16:01:23 +0530

    #

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -6,6 +6,7 @@ import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:study_sync/models/user_model.dart';
 import 'package:study_sync/providers/auth_provider.dart';
+import 'package:study_sync/screens/BottomNavigation.dart';
 import 'package:study_sync/screens/home/home_screen.dart';
 import 'package:study_sync/screens/auth/googleInfo_screen.dart';
 import 'package:study_sync/screens/auth/rememberMe_screen.dart';
@@ -378,7 +379,7 @@ class _LoginScreenState extends State<LoginScreen> {
                                   }
                                   Navigator.of(context).pushReplacement(
                                     MaterialPageRoute(
-                                      builder: (context) => const HomeScreen(),
+                                      builder: (context) => const Bottomnavigation(),
                                     ),
                                   );
                                 } else {
