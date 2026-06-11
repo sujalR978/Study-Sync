2b5b243cbde826f187d7cfb5f93d3086b4a75ac7
sujalR978 <srashiya955@rku.ac.in>
2026-06-11 16:46:06 +0530

    #

--- lib/widgets/Ai_answer.dart
+++ lib/widgets/Ai_answer.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+
 import 'package:study_sync/constants/app_colors.dart';
 
 
