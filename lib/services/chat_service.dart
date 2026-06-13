d158d4690d3c7932eaf7f45c7c41f124b4c95649
sujalR978 <srashiya955@rku.ac.in>
2026-06-13 16:36:51 +0530

    #

---
+++ lib/services/chat_service.dart
@@ -0,0 +1,28 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:study_sync/models/chat_model.dart';
+
+class ChatService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<void> addChat({
+    required String roal,
+    required String content,
+    required Timestamp timestamp,
+  }) async {
+    String uid = _auth.currentUser!.uid;
+
+    ChatModel chat = ChatModel(
+      roal: roal,
+      content: content,
+      timestamp: timestamp,
+    );
+
+    await _firestore
+        .collection('users')
+        .doc(uid)
+        .collection('chats')
+        .add(chat.toMap());
+  }
+}
