6c57ff94509e98274d9bfc0df02ef9f886602e38
sujalR978 <srashiya955@rku.ac.in>
2026-06-13 16:28:05 +0530

    #

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,22 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class ChatModel {
+  String roal;
+  String content;
+  Timestamp timestamp;
+
+  ChatModel({
+    required this.roal,
+    required this.content,
+    required this.timestamp,
+  });
+
+  Map<String, String> toMap() {
+    return {
+      'roal': roal,
+      'content': content,
+      'timestamp': timestamp.toString(),
+    };
+  }
+}
