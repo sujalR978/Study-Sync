57c7fa09b190436d9e75182fa18bd62f24def2c7
sujalR978 <srashiya955@rku.ac.in>
2026-06-13 10:11:59 +0530

    #

--- lib/API/get_open_router_response.dart
+++ lib/API/get_open_router_response.dart
@@ -1,5 +1,7 @@
 import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:image_picker/image_picker.dart';
 import 'package:study_sync/API/api_key.dart';
 
 Future<String> getOpenRouterResponse(List<Map<String, String>> massage) async {
@@ -19,6 +21,50 @@ Future<String> getOpenRouterResponse(List<Map<String, String>> massage) async {
 
   final response = await http.post(
     Uri.parse(endpoint),
+    headers: headers,
+    body: body,
+  );
+
+  if (response.statusCode == 200) {
+    final data = jsonDecode(response.body);
+
+    return data['choices'][0]['message']['content'];
+  } else {
+    throw Exception('Failed: ${response.statusCode} - ${response.body}');
+  }
+}
+
+Future<dynamic> getOpenRouterResponseForGpt40(
+  String userInput,
+  List<XFile> images,
+) async {
+  const String endPoint = "https://openrouter.ai/api/v1/chat/completions";
+
+  final headers = {
+    'Authorization': 'Bearer $API_image',
+    'Content-Type': 'application/json',
+  };
+
+  final body = jsonEncode({
+    "model": "anthropic/claude-sonnet-4",
+    "messages": [
+      {
+        "role": "user",
+        "content": [
+          {"type": "text", "text": userInput},
+          {
+            "type": "image_url",
+            "image_url": {"url": images},
+          },
+        ],
+      },
+    ],
+    "temperature": 0.7,
+    "max_tokens": 200,
+  });
+
+  final response = await http.post(
+    Uri.parse(endPoint),
     headers: headers,
     body: body,
   );
