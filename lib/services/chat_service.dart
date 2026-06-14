1449a0e41e8fdbd662e2d054e2a39337aca5d1a1
sujalR978 <srashiya955@rku.ac.in>
2026-06-14 14:51:57 +0530

    #

--- lib/services/chat_service.dart
+++ lib/services/chat_service.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/material.dart';
+
 import 'package:study_sync/models/chat_image_model.dart';
 import 'package:study_sync/models/chat_model.dart';
 
