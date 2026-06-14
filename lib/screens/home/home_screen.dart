7dde19d020134de721f65950be19872ffa300374
sujalR978 <srashiya955@rku.ac.in>
2026-06-14 15:36:24 +0530

    #

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -985,7 +985,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 },
               ),
             ),
-            
+            Text('Notes'),
           ],
         ),
       ),
