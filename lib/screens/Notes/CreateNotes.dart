41692d7ce53cf907123d5d21cff699300afbfaaf
sujalR978 <srashiya955@rku.ac.in>
2026-06-15 16:35:14 +0530

    #

--- lib/screens/Notes/CreateNotes.dart
+++ lib/screens/Notes/CreateNotes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:study_sync/screens/Notes/Notes.dart';
 
 class Createnotes extends StatefulWidget {
   const Createnotes({super.key});
@@ -10,6 +11,25 @@ class Createnotes extends StatefulWidget {
 class _CreatenotesState extends State<Createnotes> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(
+              context,
+            ).push(MaterialPageRoute(builder: (context) => Notes()));
+          },
+          icon: Icon(Icons.arrow_back_ios),
+        ),
+
+        title: Padding(
+          padding: const EdgeInsets.only(left: 110),
+          child: Text(
+            'Add Notes',
+            style: TextStyle(fontWeight: FontWeight.w500),
+          ),
+        ),
+      ),
+    );
   }
 }
