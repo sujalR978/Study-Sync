fe7d88123e9ed594b3cd4be0942f4938db6abf50
sujalR978 <srashiya955@rku.ac.in>
2026-06-15 17:02:25 +0530

    #

--- lib/services/Notes_service.dart
+++ lib/services/Notes_service.dart
@@ -1,13 +1,20 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:study_sync/models/Notes_model.dart';
 
 class NotesService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
   Future<void> AddNotes({required String title, required String body}) async {
+    String uid = _auth.currentUser!.uid;
 
+    NotesModel notes = NotesModel(title: title, body: body);
 
-    
+    await _firestore
+        .collection('users')
+        .doc(uid)
+        .collection('notes')
+        .add(notes.toMap());
   }
 }
