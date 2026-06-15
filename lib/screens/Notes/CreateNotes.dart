57339ea9dae91ffa829cf8be262c430cdfd0551f
sujalR978 <srashiya955@rku.ac.in>
2026-06-15 16:31:59 +0530

    #

---
+++ lib/screens/Notes/CreateNotes.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Createnotes extends StatefulWidget {
+  const Createnotes({super.key});
+
+  @override
+  State<Createnotes> createState() => _CreatenotesState();
+}
+
+class _CreatenotesState extends State<Createnotes> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
