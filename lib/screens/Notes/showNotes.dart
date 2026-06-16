a01509fecf37f66e8f26d66ac96f79fd9b6ce4ec
sujalR978 <srashiya955@rku.ac.in>
2026-06-16 19:07:23 +0530

    #

--- lib/screens/Notes/showNotes.dart
+++ lib/screens/Notes/showNotes.dart
@@ -1,19 +1,35 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Shownotes extends StatefulWidget {
- final String taskid;
-  const Shownotes({super.key,required this.taskid});
+  final String noteId;
+  const Shownotes({super.key, required this.noteId});
 
   @override
   State<Shownotes> createState() => _ShownotesState();
 }
 
 class _ShownotesState extends State<Shownotes> {
+  Future<DocumentSnapshot> showNotes() async {
+    return await FirebaseFirestore.instance
+        .collection('users')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .collection('notes')
+        .doc(widget.noteId)
+        .get();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      
-
+    return Column(
+      children: [
+        FutureBuilder(
+          future: showNotes(),
+          builder: (context, snepshot) {
+            return Container(child: Column(children: []));
+          },
+        ),
       ],
     );
   }
