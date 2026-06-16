726969518569a318d8b8f408199050ae07bb3e5c
sujalR978 <srashiya955@rku.ac.in>
2026-06-16 19:02:20 +0530

    #

---
+++ lib/screens/Notes/showNotes.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Shownotes extends StatefulWidget {
+ final String taskid;
+  const Shownotes({super.key});
+
+  @override
+  State<Shownotes> createState() => _ShownotesState();
+}
+
+class _ShownotesState extends State<Shownotes> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+
+
+      ],
+    );
+  }
+}
