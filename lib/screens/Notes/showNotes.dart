346c0d886170e41a5272599e8fd93c24f8750f0d
sujalR978 <srashiya955@rku.ac.in>
2026-06-16 19:02:32 +0530

    #

--- lib/screens/Notes/showNotes.dart
+++ lib/screens/Notes/showNotes.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class Shownotes extends StatefulWidget {
  final String taskid;
-  const Shownotes({super.key});
+  const Shownotes({super.key,required this.taskid});
 
   @override
   State<Shownotes> createState() => _ShownotesState();
@@ -12,7 +12,7 @@ class _ShownotesState extends State<Shownotes> {
   @override
   Widget build(BuildContext context) {
     return Column(children: [
-
+      
 
       ],
     );
