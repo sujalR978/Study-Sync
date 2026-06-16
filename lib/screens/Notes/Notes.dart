481bbed19571c934212bd3d441f89865ad94830b
sujalR978 <srashiya955@rku.ac.in>
2026-06-16 18:30:02 +0530

    #

--- lib/screens/Notes/Notes.dart
+++ lib/screens/Notes/Notes.dart
@@ -190,7 +190,7 @@ class _NotesState extends State<Notes> {
                                       ),
                                     ),
 
-                                    SizedBox(: 38),
+                                    SizedBox(width: 35),
                                     Align(
                                       child: IconButton(
                                         onPressed: () {
