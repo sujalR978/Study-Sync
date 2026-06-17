2303ca2ad67f50eacb9cb9f433ca7704b36ca002
sujalR978 <srashiya955@rku.ac.in>
2026-06-17 17:04:02 +0530

    #

--- lib/screens/Notes/EditNotes.dart
+++ lib/screens/Notes/EditNotes.dart
@@ -1,24 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class Editnotes extends StatefulWidget {
-  const Editnotes({super.key});
+  final String noteId;
+  const Editnotes({super.key, required this.noteId});
 
   @override
   State<Editnotes> createState() => _EditnotesState();
 }
 
 class _EditnotesState extends State<Editnotes> {
+  Future<DocumentSnapshot> shownotes() async {
+    return await FirebaseFirestore.instance
+        .collection('users')
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .collection('notes')
+        .doc(widget.noteId)
+        .get();
+  }
 
-
-  
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
+    return Scaffold(body: Column(children: [
 
         ],
-      ),
-    );
+      ));
   }
 }
