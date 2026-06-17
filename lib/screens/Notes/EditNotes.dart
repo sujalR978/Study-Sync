1ba8b4d24bf9d6828a8f253dfad726ab48cc4330
sujalR978 <srashiya955@rku.ac.in>
2026-06-17 16:56:43 +0530

    #

---
+++ lib/screens/Notes/EditNotes.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Editnotes extends StatefulWidget {
+  const Editnotes({super.key});
+
+  @override
+  State<Editnotes> createState() => _EditnotesState();
+}
+
+class _EditnotesState extends State<Editnotes> {
+
+
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
