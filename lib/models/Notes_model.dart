9e3aa3b48ec58cf85300108af3ecfd26456a93e6
sujalR978 <srashiya955@rku.ac.in>
2026-06-17 16:58:03 +0530

    #

--- lib/models/Notes_model.dart
+++ lib/models/Notes_model.dart
@@ -15,3 +15,22 @@ class NotesModel {
     return {'title': title, 'body': body, 'id': id, 'order': order};
   }
 }
+
+
+class EditNotesModel {
+  final String title;
+  final String body;
+  final String id;
+  final int order;
+
+  NotesModel({
+    required this.title,
+    required this.body,
+    required this.id,
+    required this.order,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {'title': title, 'body': body, 'id': id, 'order': order};
+  }
+}
