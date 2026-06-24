b554c22f6f53d2787df153318c9c3cc38f3060d8
sujalR978 <srashiya955@rku.ac.in>
2026-06-24 15:34:17 +0530

    #

--- lib/screens/BottomNavigation.dart
+++ lib/screens/BottomNavigation.dart
@@ -14,35 +14,62 @@ class Bottomnavigation extends StatefulWidget {
 
 class _MainState extends State<Bottomnavigation> {
   int currentIndex = 0;
+  final List<GlobalKey<NavigatorState>> _navigatorKeys = [
+    GlobalKey<NavigatorState>(),
+    GlobalKey<NavigatorState>(),
+    GlobalKey<NavigatorState>(),
+    GlobalKey<NavigatorState>(),
+  ];
 
-  final List<Widget> page = [
-    const HomeScreen(),
-    const Notes(),
-    const Profilescreen(),
-    const SettingScreen(),
-  ]; 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: page[currentIndex],
-
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: currentIndex,
-        onTap: (value) {
-          setState(() {
-            currentIndex = value;
-          });
-        },
-        backgroundColor: Colors.white,
-        selectedItemColor: const Color.fromARGB(255, 89, 33, 243),
-        unselectedItemColor: Colors.grey,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.notes), label: "Notes"),
-          BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
-          BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Setting"),
-        ],
+    return WillPopScope(
+      onWillPop: () async {
+        if (_navigatorKeys[currentIndex].currentState?.canPop() ?? false) {
+          _navigatorKeys[currentIndex].currentState?.pop();
+          return false;
+        }
+        return true;
+      },
+      child: Scaffold(
+        body: IndexedStack(
+          index: currentIndex,
+          children: [
+            _buildNavigator(0, const HomeScreen()),
+            _buildNavigator(1, const Notes()),
+            _buildNavigator(2, const Profilescreen()),
+            _buildNavigator(3, const SettingScreen()),
+          ],
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          currentIndex: currentIndex,
+          onTap: (value) {
+            setState(() {
+              currentIndex = value;
+            });
+          },
+          backgroundColor: Colors.white,
+          selectedItemColor: const Color.fromARGB(255, 89, 33, 243),
+          unselectedItemColor: Colors.grey,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+            BottomNavigationBarItem(icon: Icon(Icons.notes), label: "Notes"),
+            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+            BottomNavigationBarItem(icon: Icon(Icons.settings), label: "Setting"),
+          ],
+        ),
       ),
+    );
+  }
+
+  Widget _buildNavigator(int index, Widget initialPage) {
+    return Navigator(
+      key: _navigatorKeys[index],
+      onGenerateRoute: (RouteSettings settings) {
+        return MaterialPageRoute(
+          builder: (context) => initialPage,
+        );
+      },
     );
   }
 }
